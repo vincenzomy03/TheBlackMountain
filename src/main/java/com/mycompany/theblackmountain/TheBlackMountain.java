8371fb30820a96776274a1405d6fd8aa5bb2f960
vinc <vincenzo.my03@gmail.com>
2025-04-16 20:52:34 +0200

    creazione main file

---
+++ src/main/java/com/mycompany/theblackmountain/TheBlackMountain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.theblackmountain;
+
+/**
+ *
+ * @author vince
+ */
+public class TheBlackMountain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
